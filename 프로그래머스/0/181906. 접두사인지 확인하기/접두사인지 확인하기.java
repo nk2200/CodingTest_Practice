3e26029d1896c0110f12633d474fc8baf6067c0b
nickie <71312414+nk2200@users.noreply.github.com>
2025-09-03 14:29:23 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.05 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        for(int i=0;i<my_string.length();i++){
+            if(is_prefix.equals(my_string.substring(0,i))){
+                answer = 1;
+            }
+            //System.out.println(is_prefix+"/"+my_string.substring(0, i));
+        }
+        return answer;
+    }
+}
